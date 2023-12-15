a61e9dd1a5c24d13b933e5505204cb4664cd61c3
opshanka <opshankaprabath@Opshankas-MacBook-Pro.local>
2023-12-15 22:42:43 +0530

    currency converter application final

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -1,20 +1,92 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
+import axios from "axios"
+
 
 export default function MainPage() {
+    const [date,setDate] = useState(null);
+    const [sourceCurrency,setSourceCurrency] = useState("");
+    const [targetCurrency,setTargetCurrency] = useState("");
+    const [amountSourceCurrency,setAmountSourceCurrency] = useState(0);
+    const [amountTargetCurrency,setAmountTargetCurrency] = useState(0);
+const [currencyNames,setCurrencyNames] = useState([]);
+const [loading,setLoading] = useState(true);
+
+    //handle submit
+    const handleSubmit = async (e)=>{
+        e.preventDefault();
+        try{
+            const responce = await axios.get("http://localhost:3000/convert", {params:{
+                date,
+                sourceCurrency,
+                targetCurrency,
+                amountSourceCurrency,
+            }});
+
+            setAmountTargetCurrency(responce.data)
+            setLoading(false)
+
+        //TOD: set the rest....
+        }catch{
+            console.log(sourceCurrency);
+        }
+    }
+
+    useEffect(()=>{
+        const getCurrencyNames = async() => {
+            try {
+                const response = await axios.get("http://localhost:3000/getAllCurrencies")
+                setCurrencyNames(response.data);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        getCurrencyNames();
+    },[]);
+
+
     return (
         <div>
             <h1 className='lg:mx-32 text-5xl font-bold text-green-500'> Money is everything</h1>
             <p className='lg:mx-32 opacity-40 py-3'> Convert Your currency in real time</p>
             <div className='mt-5 flex items-center justify-center'>
                 <section className='w-full lg:w-1/2'>
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <div className="mb-5">
-                            <label for="date" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Date</label>
-                            <input type="date" id="date" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="" required />
+                            <label htmlFor={date} className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Date</label>
+                            <input type="date" id={date} onChange={(e)=> setDate(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="" required />
                         </div>
+                        <div className="mb-5">
+                            <label htmlFor={sourceCurrency} className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Source Currency</label>
+                            <select id={sourceCurrency} onChange={(e) => setSourceCurrency(e.target.value)} name={sourceCurrency} value={sourceCurrency} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500">
+                                <option selected>Select Source Currency</option>
+                                {Object.keys(currencyNames).map((currency)=>(<option className='p-1' key={currency} value={currency}>{currencyNames[currency]}</option>))}
+                            </select>
+                        </div>
+                        <div className="mb-5">
+                            <label htmlFor={targetCurrency}  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Target Currency</label>
+                            <select id={targetCurrency} name={targetCurrency} value={targetCurrency} onChange={(e) => setTargetCurrency(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500">
+                                <option selected>Select Target Currency</option>
+                                {Object.keys(currencyNames).map((currency)=>(<option className='p-1' key={currency} value={currency}>{currencyNames[currency]}</option>))}
+                  
+                            </select>
+                        </div>
+                        <div className="mb-5">
+                            <label htmlFor={amountSourceCurrency}  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Amount In Source Currency</label>
+                            <input type='number' id={amountSourceCurrency} onChange={(e) => setAmountSourceCurrency(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="" required />
+                        </div>
+                        <button className='bg-green-600 hover:bg-green-700 text-white font-medium py-2 px-4 rounded-md'> Get the target Currency</button>
                     </form>
                 </section>
+              
             </div>
+
+            {!loading ?
+            <section className='lg:mx-60 text-xl mt-5'>
+            {amountTargetCurrency} {currencyNames[sourceCurrency]} is equal to <span className='text-green-500 font-bold'>
+                 {amountTargetCurrency}
+                 </span> {currencyNames[targetCurrency]}
+
+            </section> : null }
         </div>
     )
 }
--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,56 @@
-console.log("hello world")
+const express = require("express");
+const cors = require("cors");
+const axios = require('axios')
+
+const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+
+//all currencies
+app.get("/getAllCurrencies", async (req, res) => {
+    const namesUrl = "https://openexchangerates.org/api/currencies.json?prettyprint=false&show_alternative=false&show_inactive=false&app_id=06114aadc7604cc8b216e7bc19488a24"
+
+    try {
+        const namesResponse = await axios.get(namesUrl);
+        const nameData = namesResponse.data;
+        return res.json(nameData);
+    } catch (error) {
+        console.error(error)
+    }
+});
+
+//get the target amount
+app.get("/convert", async (req, res) => {
+    const { date,
+        sourceCurrency,
+        targetCurrency,
+        amountSourceCurrency } = req.query;
+    const namesUrl = `https://openexchangerates.org/api/historical/${date}.json?app_id=dca16a93572c42aca2b4d21d0d567cb5`
+    try {
+        const dataResponse = await axios.get(namesUrl);
+        const rates = dataResponse.data.rates;
+
+        const sourceRate = rates[sourceCurrency];
+        const targetRate = rates[targetCurrency];
+
+        const targetAmount = (targetRate / sourceRate) * amountSourceCurrency;
+        return res.json(targetAmount.toFixed(2));
+
+    } catch (error) {
+        console.error(error)
+    }
+});
+
+//listen to a port
+app.listen(3000, () => {
+    console.log("SERVER STARTED");
+});
+
+// "AED": 3.672914,
+//     "AFN": 48.337601,
+
+// x = (ERN/AED) * 3.672975;
+
+
